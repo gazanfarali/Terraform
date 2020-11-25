1a02794b9e8656b17450c57f2cac14f174387b78
syed <gasyed@live.com>
2020-11-25 11:45:50 +0000

    adding instructions fields to file

--- ec2-key-ipadd/main.tf
+++ ec2-key-ipadd/main.tf
@@ -3,14 +3,15 @@ provider "aws" {
   region  = "eu-west-2"
 }
 
-
+# creating EC2 instance with existing aws key pair
 resource "aws_instance" "viaterraform" {
     ami     =  "ami-06178cf087598769c"
     instance_type = "t2.micro"
-    key_name  = "Faisal-07-2020"
+    key_name  = "Faisal-07-2020" # change this to yours existing aws key_pair
 
 }
 
+# creating function to get ip address as output, this will be correspond to output.tf file.
 resource "aws_eip" "ip" {
   vpc      = true
   instance = aws_instance.viaterraform.id
--- ec2-key-ipadd/output.tf
+++ ec2-key-ipadd/output.tf
@@ -1,6 +1,8 @@
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/eip
+
 output "ip" {
   value = aws_eip.ip.public_ip
-  description = "The private IP address of the main server instance."
+  description = "The public IP address of the main server instance."
 }
 
 
